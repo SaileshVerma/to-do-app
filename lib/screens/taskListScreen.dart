d3c34406b7fc49474d7e310b87e6efd67d61f748
Sailu <vermasailesh271@gmail.com>
2021-12-20 15:56:16 +0530

    added animatedList

--- lib/screens/taskListScreen.dart
+++ lib/screens/taskListScreen.dart
@@ -1,8 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todo/providers/taskProvider.dart';
+import '../providers/taskProvider.dart';
 
 import '../models/taskmodel.dart';
 import '../widget/emptyScreenText.dart';
@@ -19,6 +17,29 @@ class TaskListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final taskprovider = Provider.of<TaskProvider>(context);
+
+    void deleteItem(String id, int index) {
+      listkey.currentState!.removeItem(
+        index,
+        (context, animation) =>
+            // Text("data"),
+            TaskGrid(
+          id: id,
+          index: index,
+          animation: animation,
+          deleteItem: (id, index) {},
+        ),
+        duration: Duration(milliseconds: 500),
+      );
+      // taskDataList.removeAt(index);
+      // taskprovider.deleteTask(id);
+      // print("runn ?>>>>>>>>>>");
+      //
+    }
+
+    print("runn ?>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" +
+        taskDataList.length.toString());
     return Container(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -27,9 +48,13 @@ class TaskListScreen extends StatelessWidget {
             : AnimatedList(
                 key: listkey,
                 physics: BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics()),
+                  parent: AlwaysScrollableScrollPhysics(),
+                ),
                 initialItemCount: taskDataList.length,
                 itemBuilder: (ctx, i, animation) => TaskGrid(
+                  index: i,
+                  animation: animation,
+                  deleteItem: deleteItem,
                   id: taskDataList[i].id,
                 ),
               ),
--- lib/widget/taskGrid.dart
+++ lib/widget/taskGrid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 
 import '../widget/softButton.dart';
@@ -12,9 +13,16 @@ import 'addEditTaskbox.dart';
 
 class TaskGrid extends StatelessWidget {
   final String id;
+  final int index;
+
+  final Animation<double> animation;
+  final Function(String id, int index) deleteItem;
 
   TaskGrid({
     required this.id,
+    required this.index,
+    required this.animation,
+    required this.deleteItem,
   });
 
   @override
@@ -22,141 +30,147 @@ class TaskGrid extends StatelessWidget {
     final taskprovider = Provider.of<TaskProvider>(context);
     final item =
         taskprovider.taskdata.firstWhere((element) => element.id == id);
-    return InkWell(
-      onTap: () => Navigator.of(context).push(
-        MaterialPageRoute(
-          // navigating to task's subtask screen
-          builder: (ctx) => TaskDetailScreen(
-            id: id,
+    return SizeTransition(
+      sizeFactor: animation,
+      child: InkWell(
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            // navigating to task's subtask screen
+            builder: (ctx) => TaskDetailScreen(
+              id: id,
+            ),
           ),
         ),
-      ),
-      child: Column(
-        children: [
-          Material(
-            elevation: 7,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    item.isActive ? Colors.white30 : Colors.grey.shade300,
-                    Colors.blueGrey.shade50,
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Checkbox(
-                    checkColor: Colors.white,
-                    activeColor: Colors.black26,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    value: item.isActive,
-                    onChanged: (val) {
-                      taskprovider.changeStatus(id);
-                    },
+        child: Column(
+          children: [
+            Material(
+              elevation: 7,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      item.isActive ? Colors.white30 : Colors.grey.shade300,
+                      Colors.blueGrey.shade50,
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      bottom: 60,
-                      top: 30,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      activeColor: Colors.black26,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      value: item.isActive,
+                      onChanged: (val) {
+                        taskprovider.changeStatus(id);
+                      },
                     ),
-                    child: Container(
-                      width: 145,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 60,
+                        top: 30,
+                      ),
+                      child: Container(
+                        width: 145,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              item.title,
+                              style: TextStyle(
+                                decoration: item.isActive
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              item.description.length > 24
+                                  ? hidetext(item.description) + "..."
+                                  : item.description,
+                              style: TextStyle(
+                                decoration: item.isActive
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                                color: Colors.black45,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 22.0),
+                      child: Row(
                         children: [
-                          Text(
-                            item.title,
-                            style: TextStyle(
-                              decoration: item.isActive
-                                  ? TextDecoration.lineThrough
-                                  : TextDecoration.none,
-                              color: Colors.black87,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                          SoftButton(
+                            color: Colors.grey.shade300,
+                            func: () {
+                              //delete task
+                              //   taskprovider.deleteTask(id);
+                              deleteItem(id, index);
+                              //   print("runn ?>>>>>>>>>>");
+                              // ScaffoldMessenger.of(context).showSnackBar(
+                              //   SnackBar(
+                              //     duration: Duration(seconds: 1),
+                              //     content:
+                              //         const Text("task deleted successfully"),
+                              //   ),
+                              // );
+                            },
+                            icon: const Icon(
+                              Icons.delete_outline,
+                              color: Colors.black38,
                             ),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            item.description.length > 24
-                                ? hidetext(item.description) + "..."
-                                : item.description,
-                            style: TextStyle(
-                              decoration: item.isActive
-                                  ? TextDecoration.lineThrough
-                                  : TextDecoration.none,
-                              color: Colors.black45,
-                              fontSize: 12,
+                          SoftButton(
+                            color: Colors.grey.shade300,
+                            icon: const Icon(
+                              Icons.edit,
+                              color: Colors.black38,
                             ),
+                            func: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => Container(
+                                  child: AddEditTaskBox(
+                                    receivedTitle: item.title,
+                                    receivedDesc: item.description,
+                                    addeditDescription: (title, desc) {
+                                      taskprovider.editTask(
+                                          item.id, title, desc);
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 22.0),
-                    child: Row(
-                      children: [
-                        SoftButton(
-                          color: Colors.grey.shade300,
-                          func: () {
-                            taskprovider.deleteTask(id);
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: Duration(seconds: 1),
-                                content:
-                                    const Text("task deleted successfully"),
-                              ),
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.delete_outline,
-                            color: Colors.black38,
-                          ),
-                        ),
-                        SoftButton(
-                          color: Colors.grey.shade300,
-                          icon: const Icon(
-                            Icons.edit,
-                            color: Colors.black38,
-                          ),
-                          func: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => Container(
-                                child: AddEditTaskBox(
-                                  receivedTitle: item.title,
-                                  receivedDesc: item.description,
-                                  addeditDescription: (title, desc) {
-                                    taskprovider.editTask(item.id, title, desc);
-                                  },
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
